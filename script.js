ecd533f64995495ed65d9ca1c72234946639e884
sheready <csstlw@gmail.com>
2023-07-15 14:48:10 +0000

    add class calculator

--- script.js
+++ script.js
@@ -6,3 +6,55 @@ const allClearButton = document.querySelector('[data-all-clear]');
 const previousOperand = document.querySelector('[data-previous-operand]');
 const currentOperand = document.querySelector('[data-current-operand]');
 
+
+numberButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    calculator.appendNumber(button.innerText)
+    calculator.updateDisplay()
+  })
+})
+
+operationButtons.forEach(button => {
+  button.addEventLIstener('click', () => {
+    calculator.chooseOperation(button.innerText)
+    calculator.updateDisplay()
+  })
+})
+
+class Calculator{
+  constructor(previousOperand, currentOperand){
+    this.previousOperand = previousOperand
+    this.currentOperand - currentOperand
+    this.clear()
+  }
+
+  //specify what to clear
+  clear(){
+    this.currentOperand = ''
+    this.previousOperand = ''
+    this.operation = undefined
+  }
+
+  delete(){
+    this.currentOperand = this.currentOperand.toString().slice(0, -1)
+  }
+
+  appendNumber(number){
+    if(number === '.'  && this.currentOperand.includes('.')){
+      return this.currentOperand = this.currentOperand.toString() + number.toString()
+    }    
+  }
+
+  chooseOperation(operation){
+    if(this.currentOperand === '')
+      if(this.previousOperand !== ''){
+        return this.compute()
+      }
+      this.operation = operation
+      this.previousOperand = this.currentOperand
+      this.currentOperand = ''
+  
+  }
+}
+
+const calculator = new Calculator(previousOperand, currentOperand)
